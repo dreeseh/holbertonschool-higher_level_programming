633db8350370b623f1a73bcb1d6538c2946cf1d2
dreeseh <1859@holbertonschool.com>
2020-09-01 17:42:43 +0000

    changed zero to 0

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -9,6 +9,6 @@ if number < 0:
 if number > 5:
     print(number, "and is greater than 5")
 if number == 0:
-    print(number, "and is zero")
+    print(number, "and is 0")
 if number < 6 and number != 0:
     print(number, "and is less than 6 and not 0")
