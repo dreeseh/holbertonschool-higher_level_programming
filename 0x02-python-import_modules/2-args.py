56b9f3047b8c54f66a6085f53d992fa1b4e8aa70
dreeseh <1859@holbertonschool.com>
2020-09-04 01:06:31 +0000

    added :d and :s to {}

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -5,8 +5,8 @@ if __name__ == "__main__":
     if n == 1:
         print("0: arguments.")
     elif n == 2:
-        print("{} argument:".format(n - 1))
+        print("{:d} argument:".format(n - 1))
     else:
-        print("{} arguments:".format(n - 1))
+        print("{:d} arguments:".format(n - 1))
     for i in range(1, n):
-        print("{}: {}".format(i, argv[i]))
+        print("{:d}: {:s}".format(i, argv[i]))
