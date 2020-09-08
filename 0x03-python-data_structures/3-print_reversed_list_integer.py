7eb8305ace29c4e78890cbcfcdb473e5f2499010
dreeseh <1859@holbertonschool.com>
2020-09-08 17:12:29 +0000

    added null check & used .format

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
+    if (my_list) = None:
+        return (None)
     list.reverse(my_list)
     for i in range(len(my_list)):
-        print(my_list[i])
+        print("{:d}".format(my_list[i]))
