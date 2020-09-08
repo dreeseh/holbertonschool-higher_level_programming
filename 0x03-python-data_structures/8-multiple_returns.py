2104f322aa2131aa4bb3db7c3f8725f766902e2b
dreeseh <1859@holbertonschool.com>
2020-09-08 18:14:19 +0000

    changed len to length

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -2,7 +2,7 @@
 def multiple_returns(sentence):
     list = [0, 1]
     length = len(sentence)
-    if len == 0:
+    if length == 0:
         list[0] = len(sentence)
         list[1] = None
     else:
