28ae11b57f67c3b8d1a1bdf2fb05f2da8cc0f08e
dreeseh <1859@holbertonschool.com>
2020-09-08 17:28:27 +0000

    now returning copy of list

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,7 +1,7 @@
 def new_in_list(my_list, idx, element):
-    if idx < 0 or idx > len(my_list):
-        return (my_list)
     reeses_list = my_list
+    if idx < 0 or idx >= len(reeses_list):
+        return (reeses_list)
     reeses_list = list(reeses_list)
     reeses_list[idx] = element
     return (reeses_list)
