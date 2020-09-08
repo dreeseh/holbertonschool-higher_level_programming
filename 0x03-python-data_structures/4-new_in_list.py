5f5f302e37e29b9cbbaad54b04f0ebfeb736fb2d
dreeseh <1859@holbertonschool.com>
2020-09-08 17:33:16 +0000

    moved list() to top

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,7 +1,6 @@
 def new_in_list(my_list, idx, element):
-    reeses_list = my_list
+    reeses_list = list(my_list)
     if idx < 0 or idx >= len(reeses_list):
         return (reeses_list)
-    reeses_list = list(reeses_list)
     reeses_list[idx] = element
     return (reeses_list)
