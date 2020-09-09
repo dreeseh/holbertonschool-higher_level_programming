b967dfa85a6f45db71b5ff2ad2e306549b8cd912
dreeseh <1859@holbertonschool.com>
2020-09-09 02:24:58 +0000

    added mylist[i] % 2

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -2,7 +2,7 @@
 def divisible_by_2(my_list=[]):
     reeses_list = my_list.copy()
     for i in range(len(my_list)):
-        if i % 2 == 0:
+        if reeses_list[i] % 2 == 0:
             reeses_list[i] = True
         else:
             reeses_list[i] = False
