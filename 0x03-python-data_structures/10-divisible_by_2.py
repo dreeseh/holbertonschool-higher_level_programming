e651c4a48e07b4bc0c6458c2ab2febd577685b0e
dreeseh <1859@holbertonschool.com>
2020-09-09 02:17:41 +0000

    changed copy(my_list to my_list.copy())

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 def divisible_by_2(my_list=[]):
-    reeses_list = []
-    reeses_list = list(my_list)
+    reeses_list = my_list.copy()
     for i in my_list:
         if i % 2 == 0:
             reeses_list[i] = True
