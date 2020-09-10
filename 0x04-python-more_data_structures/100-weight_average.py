855b4d219ba3a7909b85791c91a75887bf647069
dreeseh <1859@holbertonschool.com>
2020-09-10 19:14:39 +0000

    pep8

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -3,5 +3,5 @@ def weight_average(my_list=[]):
     if len(my_list) == 0:
         return 0
     weighted = sum(i[0] * i[1] for i in my_list)
-    positions = sum (j[1] for j in my_list)
+    positions = sum(j[1] for j in my_list)
     return weighted / positions
