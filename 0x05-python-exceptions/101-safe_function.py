d14d22ab1cf5516db9895746f79b439404f40f8a
dreeseh <1859@holbertonschool.com>
2020-09-21 17:09:14 +0000

    pep8 fixes

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
 import sys
+
+
 def safe_function(fct, *args):
     try:
         return fct(*args)
 
-    except(ZeroDivisionError, ValueError, TypeError, IndexError) as reeses_error:
-        print("Exceptions: {}".format(reeses_error), file=sys.stderr)
+    except(ZeroDivisionError, ValueError, TypeError, IndexError) as reeses_err:
+        print("Exceptions: {}".format(reeses_err), file=sys.stderr)
         return None
