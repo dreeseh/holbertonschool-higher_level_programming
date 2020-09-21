54ec7e53c307153b89ecf171ab44a56f840b7959
dreeseh <1859@holbertonschool.com>
2020-09-21 17:13:06 +0000

    fixed spelling error

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -1,11 +1,9 @@
 #!/usr/bin/python3
-import sys
-
-
 def safe_function(fct, *args):
     try:
         return fct(*args)
 
     except Exception as reeses_err:
-        print("Exceptions: {}".format(reeses_err), file=sys.stderr)
+        import sys
+        print("Exception: {}".format(reeses_err), file=sys.stderr)
         return None
