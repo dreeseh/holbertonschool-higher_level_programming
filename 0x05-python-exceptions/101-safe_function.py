8b40a6eba66135c1f74bffc83c309f51c37342ff
dreeseh <1859@holbertonschool.com>
2020-09-21 17:11:35 +0000

    added Ecxeption

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -6,6 +6,6 @@ def safe_function(fct, *args):
     try:
         return fct(*args)
 
-    except(ZeroDivisionError, ValueError, TypeError, IndexError) as reeses_err:
+    except Exception as reeses_err:
         print("Exceptions: {}".format(reeses_err), file=sys.stderr)
         return None
