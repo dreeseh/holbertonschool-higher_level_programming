46f176f6f37a7473324e501b062ef4d5a8052b3b
dreeseh <1859@holbertonschool.com>
2020-09-21 17:06:36 +0000

    added blank lines after import?

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 import sys
+
+
 def safe_print_integer_err(value):
     try:
         print("{:d}".format(value))
@@ -7,4 +9,5 @@ def safe_print_integer_err(value):
 
     except Exception as reeses_exception:
         sys.stderr.write("Exception: {}\n".format(reeses_exception))
+
         return False
