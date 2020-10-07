509e7ad46574f04ef0f56ff07f1def505c7781ee
dreeseh <1859@holbertonschool.com>
2020-10-07 03:57:34 +0000

    fixed area

--- 0x0A-python-inheritance/11-square.py
+++ 0x0A-python-inheritance/11-square.py
@@ -10,15 +10,14 @@ class Square(Rectangle):
 
     def __init__(self, size):
         """initializes Square"""
-
         self.__size = size
         self.integer_validator("size", size)
         super().__init__(size, size)
 
     def area(self):
         """returns Square's area"""
-
-        return self.__size * self__width
+        return self.__size ** 2
 
     def __str__(self):
+        """creates a str obj from given obj"""
         return "[Square] {}/{}".format(self.__size, self.__size)
