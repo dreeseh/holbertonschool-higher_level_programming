578023f1d46528bfb8164fed64821cba48c931dd
dreeseh <1859@holbertonschool.com>
2020-10-07 23:08:29 +0000

    changed quiotes

--- 0x0B-python-input_output/9-add_item.py
+++ 0x0B-python-input_output/9-add_item.py
@@ -7,10 +7,10 @@ from sys import argv
 from os import path
 
 
-save_to_json_file = __import__('7-save_to_json_file').save_to_json_file
-load_from_json_file = __import__('8-load_from_json_file').load_from_json_file
+save_to_json_file = __import__("7-save_to_json_file").save_to_json_file
+load_from_json_file = __import__("8-load_from_json_file").load_from_json_file
 
-filename = 'add_item.json'
+filename = "add_item.json"
 
 if path.isfile(filename):
     my_list = load_from_json_file(filename)
