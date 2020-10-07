c33d4dad958e4f664848d804774f944e67ad1e3b
dreeseh <1859@holbertonschool.com>
2020-10-07 01:11:58 +0000

    added string to {}

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -17,10 +17,10 @@ class BaseGeometry():
     def integer_validator(self, name, value):
         """ validates value """
         if not isinstance(value, int):
-            raise TypeError("{} must be an integer".format(name))
+            raise TypeError("{:s} must be an integer".format(name))
 
         if value <= 0:
-            raise ValueError("{} must be greater than 0".format(name))
+            raise ValueError("{:s} must be greater than 0".format(name))
 
 
 class Rectangle(BaseGeometry):
