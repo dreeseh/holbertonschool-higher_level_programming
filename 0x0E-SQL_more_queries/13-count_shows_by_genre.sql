6d1c388c41967539c87adfd5199406925b27ff53
dreeseh <1859@holbertonschool.com>
2020-11-18 14:27:36 +0000

    had to change count area to have 'of' as well

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -11,7 +11,8 @@
 -- You can use only one SELECT statement
 -- The database name will be passed as an argument of the mysql command
 
-SELECT tv_genres.name AS 'genre', COUNT(tv_show_genres.genre_id) 'number_shows'
+SELECT tv_genres.name AS 'genre',
+COUNT(tv_show_genres.genre_id) 'number_of_shows'
 FROM tv_show_genres
 INNER JOIN tv_genres
 ON tv_genres.id = tv_show_genres.genre_id
