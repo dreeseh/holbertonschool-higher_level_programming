e59322e020cf7561fb4540ddfcbdb1fd5490d9a6
dreeseh <1859@holbertonschool.com>
2020-12-13 17:30:33 +0000

    sql and py files for task 4

---
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""a script that lists all cities from the database hbtn_0e_4_usa"""
+
+
+def connectToDB():
+    """connect to database"""
+    conn = MySQLdb.connect(host='localhost', port=3306,
+                           user=sys.argv[1], passwd=sys.argv[2],
+                           db=sys.argv[3], charset='utf8')
+    cur = conn.cursor()
+
+    cur.execute("SELECT cities.id, cities.name, states.name \
+                 FROM cities INNER JOIN states ON cities.state_id=states.id \
+                 ORDER BY cities.id ASC")
+
+    query_rows = cur.fetchall()
+    for row in query_rows:
+        print(row)
+
+    cur.close()
+    conn.close()
+
+if __name__ == '__main__':
+    import MySQLdb
+    import sys
+    connectToDB()
