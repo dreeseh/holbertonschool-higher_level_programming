51000cb6e1f8b8495fdc0b1f8ca5ffb6333b7114
dreeseh <1859@holbertonschool.com>
2020-12-14 00:17:20 +0000

    changed where I import

--- 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -2,12 +2,17 @@
 """
 prints all City objects from the database hbtn_0e_14_usa
 """
-
+import sys
+from model_state import Base, State
+from model_city import City
+from sqlalchemy import (create_engine)
+from sqlalchemy.orm import sessionmaker
 
 def connectToDB():
     """
     connects to DB here
     """
+
     engine = create_engine(
         'mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
             sys.argv[1],
@@ -29,9 +34,4 @@ def connectToDB():
 
 
 if __name__ == "__main__":
-    import sys
-    from model_state import Base, State
-    from model_city import City
-    from sqlalchemy import (create_engine)
-    from sqlalchemy.orm import sessionmaker
     connectToDB()
