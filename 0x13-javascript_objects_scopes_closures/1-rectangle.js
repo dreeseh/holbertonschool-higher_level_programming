fb57776cd2f9249702821f711082b1c77526711a
dreeseh <1859@holbertonschool.com>
2021-02-08 20:31:07 +0000

    task 2 and linter changes

--- 0x13-javascript_objects_scopes_closures/1-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/1-rectangle.js
@@ -2,10 +2,10 @@
 // a class Rectangle that defines a rectangle
 
 class Rectangle {
-    constructor (width, height) {
-	this.width = width;
-	this.height = height;
-    }
+  constructor (width, height) {
+    this.width = width;
+    this.height = height;
+  }
 }
 
-module.exports = Rectangle
+module.exports = Rectangle;
