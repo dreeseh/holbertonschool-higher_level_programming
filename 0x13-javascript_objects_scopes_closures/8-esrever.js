936f910a7498596b7a7db6c4a148f1367681514d
dreeseh <1859@holbertonschool.com>
2021-02-09 02:52:22 +0000

    task 8

---
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+// a function that returns the reversed version of a list
+
+exports.esrever = function (list) {
+  const newList = [];
+
+  for (let i = list.length - 1; i >= 0; i--) {
+    newList.push(list[i]);
+  }
+  return newList;
+};
