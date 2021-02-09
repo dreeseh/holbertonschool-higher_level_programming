1a56bb73b37d13649a562d07248ddcd2eab713bb
dreeseh <1859@holbertonschool.com>
2021-02-09 02:42:41 +0000

    task 7

---
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+// a function that returns the number of occurrences in a list
+
+exports.nbOccurences = function (list, searchElement) {
+  let theCounter = 0;
+  for (let i = 0; i < list.length; i++) {
+    if (list[i] === searchElement) {
+      theCounter++;
+    }
+  }
+  return (theCounter);
+};
